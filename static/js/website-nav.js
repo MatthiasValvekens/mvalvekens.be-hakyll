79c9c49c7af7eac90c06cf177ed2e162118a1107
Matthias Valvekens <matthias@mvalvekens.be>
2021-09-19 21:27:24 +0200

    Improve tab navigation on index page

--- static/js/website-nav.js
+++ static/js/website-nav.js
@@ -1,3 +1,4 @@
+// hamburger expando for navbar on a generic page
 function activateNavbarBurger() {
     const nav = document.querySelector('#main-navbar');
     const burger = nav.querySelector('.navbar-burger');
@@ -8,4 +9,42 @@ function activateNavbarBurger() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', activateNavbarBurger);
+// tab structure on index page
+
+function toggleTabNav(obj) {
+    let tabs = document.getElementById('nav-tabs');
+    let activeTabs = tabs.getElementsByClassName('is-active');
+    Array.prototype.forEach.call(activeTabs, function(tab) {
+        tab.classList.remove('is-active');
+    }
+    );
+    let targetTab = obj.getAttribute('data-tab-nav');
+    obj.parentNode.classList.add('is-active');
+    let targetObj = document.getElementById('tab-' + targetTab);
+
+    let tabview = document.getElementById('tabview');
+    let allTabContents = tabview.getElementsByClassName('content-tab')
+    Array.prototype.forEach.call(allTabContents, function(tab) {
+        tab.classList.add('is-hidden');
+    }
+    );
+    if(targetTab === "home") {
+        window.location.hash = "";
+    } else {
+        window.location.hash = "#!" + targetTab;
+    }
+    targetObj.classList.remove('is-hidden');
+}
+
+// allow linking to tabs using #!tab-name
+function handleFragment() {
+    let tabs = document.getElementById('nav-tabs');
+    let activeTabName;
+    if(window.location.hash) {
+        activeTabName = window.location.hash.substring(2);
+    } else {
+        activeTabName = "home";
+    }
+    let activeTab = tabs.querySelector('a[data-tab-nav=' + activeTabName + ']');
+    toggleTabNav(activeTab);
+}
