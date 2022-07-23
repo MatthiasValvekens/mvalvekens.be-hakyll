4cde4fc7bdb7081c704cf7889f0e1220031e5110
Matthias Valvekens <matthias@mvalvekens.be>
2022-07-23 14:13:10 +0200

    Get rid of fixed navbar
    Instead, use a regular navbar with a scroll-to-top button.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,22 @@
 
-// hamburger expando for navbar on a generic page
-function activateNavbarBurger() {
+function displayScrollToTop() {
+    var root = document.documentElement;
+    // don't bother if the screen is small
+    if (root.scrollHeight / root.clientHeight < 5) {
+        return;
+    }
+    var maxScroll = root.scrollHeight - root.clientHeight
+    var screensScrolled = root.scrollTop / root.clientHeight;
+    var button = document.getElementById("scroll-to-top");
+    button.style.display = screensScrolled > 1 ? 'block' : 'none';
+}
+
+function scrollToTop() {
+    root = document.documentElement.scrollTo(0, 0);
+}
+
+function registerNavigationElements() {
+    // hamburger expando for navbar on a generic page
     const nav = document.querySelector('#main-navbar');
     const burger = nav.querySelector('.navbar-burger');
     const menu = nav.querySelector('.navbar-menu')
@@ -8,6 +24,11 @@ function activateNavbarBurger() {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
-}
 
-document.addEventListener('DOMContentLoaded', activateNavbarBurger);
+    // scroll-to-top effect
+    document.getElementById("scroll-to-top").addEventListener("click", scrollToTop);
+}
+ 
+document.addEventListener("scroll", displayScrollToTop);
+
+document.addEventListener('DOMContentLoaded', registerNavigationElements);
